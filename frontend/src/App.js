b9553e999041c2153002c471b02a2a99361780d3
MrRIZW <iamrizwan468@gmail.com>
2026-03-19 22:51:03 +0530

    fix routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AuthPage from "./pages/AuthPage";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
-
   return (
     <BrowserRouter>
-
       <Routes>
 
         <Route path="/" element={<AuthPage />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } 
+        />
 
       </Routes>
-
     </BrowserRouter>
   );
-
 }
 
 export default App;
